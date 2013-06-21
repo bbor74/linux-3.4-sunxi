aa99c51cb3f1c6e7e1d51d77e433f84a6b55f099
George Cherian <george.cherian@ti.com>
2013-06-21 13:59:08 +0530

    usb: host: xhci-plat: release mem region while removing module
    commit 5388a3a5faba8dfa69e5f06c3a415d373c1a4316 upstream.
    
    Do a release_mem_region of the hcd resource. Without this the
    subsequent insertion of module fails in request_mem_region.
    
    Signed-off-by: George Cherian <george.cherian@ti.com>
    Acked-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -200,6 +200,8 @@ static int xhci_plat_remove(struct platform_device *dev)
 	usb_put_hcd(xhci->shared_hcd);
 
 	usb_remove_hcd(hcd);
+	iounmap(hcd->regs);
+	release_mem_region(hcd->rsrc_start, hcd->rsrc_len);
 	usb_put_hcd(hcd);
 	kfree(xhci);
 
