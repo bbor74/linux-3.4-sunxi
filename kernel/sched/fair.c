281448abc904f39c8780039bc4c570b6de46a398
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-29 11:40:16 -0700

    sched: Window-based load stat improvements
    Some tasks can have a sporadic load pattern such that they can suddenly
    start running for longer intervals of time after running for shorter
    durations. To recognize such sharp increase in tasks' demands, max
    between the average of 5 window load samples and the most recent sample
    is chosen as the task demand.
    
    Make the window size (sched_ravg_window) configurable at boot up
    time. To prevent users from setting inappropriate values for window
    size, min and max limits are defined. As 'ravg' struct tracks load for
    both real-time and non real-time tasks it is moved out of sched_entity
    struct.
    
    In order to prevent changing function signatures for move_tasks() and
    move_one_task() per-cpu variables are defined to track the total load
    moved. In case multiple tasks are selected to migrate in one load
    balance operation, loads > 100 could be sent through migration
    notifiers.
    Prevent this scenario by setting mnd.load to 100 in such cases.
    
    Define wrapper functions to compute cpu demands for tasks and to change
    rq->cumulative_runnable_avg.
    
    Change-Id: I9abfbf3b5fe23ae615a6acd3db9580cfdeb515b4
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -902,6 +902,39 @@ static inline void update_cfs_shares(struct cfs_rq *cfs_rq)
 }
 #endif /* CONFIG_FAIR_GROUP_SCHED */
 
+static inline unsigned int task_load(struct task_struct *p)
+{
+	return p->ravg.demand;
+}
+
+static inline unsigned int max_task_load(void)
+{
+	return sched_ravg_window;
+}
+
+/* Return task demand in percentage scale */
+unsigned int pct_task_load(struct task_struct *p)
+{
+	unsigned int load;
+
+	load = div64_u64((u64)task_load(p) * 100, (u64)max_task_load());
+
+	return load;
+}
+
+void init_new_task_load(struct task_struct *p)
+{
+	int i;
+	u64 wallclock = sched_clock();
+
+	p->ravg.sum			= 0;
+	p->ravg.demand			= 0;
+	p->ravg.window_start		= wallclock;
+	p->ravg.mark_start		= wallclock;
+	for (i = 0; i < RAVG_HIST_SIZE; ++i)
+		p->ravg.sum_history[i] = 0;
+}
+
 /* Only depends on SMP, FAIR_GROUP_SCHED may be removed when useful in lb */
 #if defined(CONFIG_SMP) && defined(CONFIG_FAIR_GROUP_SCHED)
 /*
@@ -4759,6 +4792,7 @@ struct lb_env {
 };
 
 static DEFINE_PER_CPU(bool, dbs_boost_needed);
+static DEFINE_PER_CPU(int, dbs_boost_load_moved);
 
 /*
  * move_task - move a task from one runqueue to another runqueue.
@@ -4862,7 +4896,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
  *
  * Called with both runqueues locked.
  */
-static int move_one_task(struct lb_env *env, int *total_run_moved)
+static int move_one_task(struct lb_env *env)
 {
 	struct task_struct *p, *n;
 
@@ -4880,9 +4914,7 @@ static int move_one_task(struct lb_env *env, int *total_run_moved)
 		 * stats here rather than inside move_task().
 		 */
 		schedstat_inc(env->sd, lb_gained[env->idle]);
-		if (sysctl_sched_ravg_window)
-			*total_run_moved += div64_u64((u64)p->se.ravg.demand *
-					100, (u64)(sysctl_sched_ravg_window));
+		per_cpu(dbs_boost_load_moved, env->dst_cpu) += pct_task_load(p);
 
 		return 1;
 	}
@@ -4900,7 +4932,7 @@ static const unsigned int sched_nr_migrate_break = 32;
  *
  * Called with both runqueues locked.
  */
-static int move_tasks(struct lb_env *env, int *total_run_moved)
+static int move_tasks(struct lb_env *env)
 {
 	struct list_head *tasks = &env->src_rq->cfs_tasks;
 	struct task_struct *p;
@@ -4942,9 +4974,7 @@ static int move_tasks(struct lb_env *env, int *total_run_moved)
 		move_task(p, env);
 		pulled++;
 		env->load_move -= load;
-		if (sysctl_sched_ravg_window)
-			*total_run_moved += div64_u64((u64)p->se.ravg.demand *
-					100, (u64)(sysctl_sched_ravg_window));
+		per_cpu(dbs_boost_load_moved, env->dst_cpu) += pct_task_load(p);
 
 #ifdef CONFIG_PREEMPT
 		/*
@@ -6090,7 +6120,6 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 			int *balance)
 {
 	int ld_moved, active_balance = 0;
-	int total_run_moved = 0;
 	struct sched_group *group;
 	unsigned long imbalance;
 	struct rq *busiest = NULL;
@@ -6107,6 +6136,7 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 
 	cpumask_copy(cpus, cpu_active_mask);
 
+	per_cpu(dbs_boost_load_moved, this_cpu) = 0;
 	schedstat_inc(sd, lb_count[idle]);
 
 redo:
@@ -6150,7 +6180,7 @@ more_balance:
 		double_rq_lock(this_rq, busiest);
 		if (!env.loop)
 			update_h_load(env.src_cpu);
-		ld_moved += move_tasks(&env, &total_run_moved);
+		ld_moved += move_tasks(&env);
 		double_rq_unlock(this_rq, busiest);
 		local_irq_restore(flags);
 
@@ -6228,13 +6258,16 @@ more_balance:
 		if (per_cpu(dbs_boost_needed, this_cpu)) {
 			struct migration_notify_data mnd;
 
-			per_cpu(dbs_boost_needed, this_cpu) = false;
-
 			mnd.src_cpu = cpu_of(busiest);
 			mnd.dest_cpu = this_cpu;
-			mnd.load = total_run_moved;
+			mnd.load = per_cpu(dbs_boost_load_moved, this_cpu);
+			if (mnd.load > 100)
+				mnd.load = 100;
 			atomic_notifier_call_chain(&migration_notifier_head,
 						   0, (void *)&mnd);
+			per_cpu(dbs_boost_needed, this_cpu) = false;
+			per_cpu(dbs_boost_load_moved, this_cpu) = 0;
+
 		}
 	}
 	if (likely(!active_balance)) {
@@ -6350,12 +6383,14 @@ static int __do_active_load_balance_cpu_stop(void *data, bool check_sd_lb_flag)
 	struct rq *busiest_rq = data;
 	int busiest_cpu = cpu_of(busiest_rq);
 	int target_cpu = busiest_rq->push_cpu;
-	int total_run_moved = 0;
 	struct rq *target_rq = cpu_rq(target_cpu);
 	struct sched_domain *sd;
 	struct task_struct *p = NULL;
 
 	raw_spin_lock_irq(&busiest_rq->lock);
+
+	per_cpu(dbs_boost_load_moved, target_cpu) = 0;
+
 #ifdef CONFIG_SCHED_HMP
 	p = busiest_rq->migrate_task;
 #endif
@@ -6406,7 +6441,7 @@ static int __do_active_load_balance_cpu_stop(void *data, bool check_sd_lb_flag)
 		schedstat_inc(sd, alb_count);
 
 		if (check_sd_lb_flag) {
-			if (move_one_task(&env, &total_run_moved))
+			if (move_one_task(&env))
 				success = true;
 		} else {
 			if (move_specific_task(&env, p))
@@ -6427,13 +6462,16 @@ out_unlock:
 	if (per_cpu(dbs_boost_needed, target_cpu)) {
 		struct migration_notify_data mnd;
 
-		per_cpu(dbs_boost_needed, target_cpu) = false;
-
 		mnd.src_cpu = cpu_of(busiest_rq);
 		mnd.dest_cpu = target_cpu;
-		mnd.load = total_run_moved;
+		mnd.load = per_cpu(dbs_boost_load_moved, target_cpu);
+		if (mnd.load > 100)
+			mnd.load = 100;
 		atomic_notifier_call_chain(&migration_notifier_head,
 					   0, (void *)&mnd);
+
+		per_cpu(dbs_boost_needed, target_cpu) = false;
+		per_cpu(dbs_boost_load_moved, target_cpu) = 0;
 	}
 	return 0;
 }
