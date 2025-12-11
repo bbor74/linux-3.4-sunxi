34538263c95e02a28d533afdad863c2755a6c6d1
Boris Belyaev <bbor1974@gmail.com>
2025-12-11 09:30:06 +0400

    ASoC: sunxi: codecdma: Fix warnings
    ../../../../../../kernel/allwinner/linux-3.4-sunxi/sound/soc/sunxi/audiocodec/sunxi_codecdma.c: In function 'sunxi_pcm_hw_params':
    ../../../../../../kernel/allwinner/linux-3.4-sunxi/sound/soc/sunxi/audiocodec/sunxi_codecdma.c:119:30: warning: assignment makes pointer from integer without a cast [enabled by default]
       substream->dma_buffer.area = 0xf0001560;//0xf0002000;  1580 ok
                                  ^
    In file included from ../../../../../../kernel/allwinner/linux-3.4-sunxi/include/linux/string.h:21:0,
                     from ../../../../../../kernel/allwinner/linux-3.4-sunxi/include/linux/bitmap.h:8,
                     from ../../../../../../kernel/allwinner/linux-3.4-sunxi/include/linux/nodemask.h:95,
                     from ../../../../../../kernel/allwinner/linux-3.4-sunxi/include/linux/mmzone.h:16,
                     from ../../../../../../kernel/allwinner/linux-3.4-sunxi/include/linux/gfp.h:4,
                     from ../../../../../../kernel/allwinner/linux-3.4-sunxi/include/linux/kmod.h:22,
                     from ../../../../../../kernel/allwinner/linux-3.4-sunxi/include/linux/module.h:13,
                     from ../../../../../../kernel/allwinner/linux-3.4-sunxi/sound/soc/sunxi/audiocodec/sunxi_codecdma.c:16:
    ../../../../../../kernel/allwinner/linux-3.4-sunxi/arch/arm/include/asm/string.h:31:16: warning: initialization makes pointer from integer without a cast [enabled by default]
        void *__p = (p); size_t __n = n;   \
                    ^
    ../../../../../../kernel/allwinner/linux-3.4-sunxi/sound/soc/sunxi/audiocodec/sunxi_codecdma.c:120:3: note: in expansion of macro 'memset'
       memset(0xf0001560, 0, 0x4000-0x00001560);
       ^

--- sound/soc/sunxi/audiocodec/sunxi_codecdma.c
+++ sound/soc/sunxi/audiocodec/sunxi_codecdma.c
@@ -116,8 +116,8 @@ static int sunxi_pcm_hw_params(struct snd_pcm_substream *substream,
 #if defined CONFIG_ARCH_SUN8IW3
 	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
 		substream->dma_buffer.addr = 0x00001560;//0x00002000;
-		substream->dma_buffer.area = 0xf0001560;//0xf0002000;  1580 ok
-		memset(0xf0001560, 0, 0x4000-0x00001560);
+		substream->dma_buffer.area = (unsigned char *)0xf0001560;//0xf0002000;  1580 ok
+		memset((void *)0xf0001560, 0, 0x4000-0x00001560);
 	}
 #endif
 //#endif
